8426d5040fef17d4c2cc7432cdd04e414f0ae255
Daniel <sosaaquinopd@gmail.com>
2026-05-16 03:22:21 -0300

    Fix: se agrega correctamente el package que rompia la estructura el proyecto

--- src/com/techlab/articulo/App.java
+++ src/com/techlab/articulo/App.java
@@ -1,13 +1,11 @@
+package com.techlab.articulo;
+
 import java.util.Scanner;
-import com.techlab.articulo.model.Articulo;
-import com.techlab.articulo.model.Categoria;
-// import com.techlab.articulo.repository.Repositorio;
-// import com.techlab.articulo.menu.MenuArticulos;
-// import com.techlab.articulo.menu.MenuCategorias;
 
 public class App {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
+        teclado.close();
     }
 }
